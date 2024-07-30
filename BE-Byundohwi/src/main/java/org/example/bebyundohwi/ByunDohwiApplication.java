e0e1c68788cdd49affbbfab2d45db9553da89bbd
ByunDohwi <dohwe0978@gmail.com>
2024-07-30 17:38:58 +0900

    add :: project config

---
+++ BE-Byundohwi/src/main/java/org/example/bebyundohwi/ByunDohwiApplication.java
@@ -0,0 +1,13 @@
+package org.example.bebyundohwi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByunDohwiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ByunDohwiApplication.class, args);
+    }
+
+}
