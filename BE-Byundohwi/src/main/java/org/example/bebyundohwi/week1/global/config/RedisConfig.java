0cc15333e11feea0b094a420d00bbc31e5b6e9ce
ByunDohwi <dohwe0978@gmail.com>
2024-08-09 11:11:05 +0900

    add :: RedisConfig

--- BE-Byundohwi/src/main/java/org/example/bebyundohwi/week1/global/config/RedisConfig.java
+++ BE-Byundohwi/src/main/java/org/example/bebyundohwi/week1/global/config/RedisConfig.java
@@ -1,4 +1,27 @@
 package org.example.bebyundohwi.week1.global.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@EnableRedisRepositories
+@Configuration
 public class RedisConfig {
+    private String host;
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<?,?> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
+    }
 }
