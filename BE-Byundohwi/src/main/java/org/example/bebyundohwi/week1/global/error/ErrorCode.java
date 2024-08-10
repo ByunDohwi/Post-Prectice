6e4c2b54eb59268f3b1fd9e0d80dbc4f88e98ba7
ByunDohwi <dohwe0978@gmail.com>
2024-08-11 01:25:15 +0900

    feat :: userNotFoundException

--- BE-Byundohwi/src/main/java/org/example/bebyundohwi/week1/global/error/ErrorCode.java
+++ BE-Byundohwi/src/main/java/org/example/bebyundohwi/week1/global/error/ErrorCode.java
@@ -9,7 +9,8 @@ public enum ErrorCode {
 
 
     USER_ALREADY_EXISTS(409, "Username Already Exists"),
-    ACCOUNTED_ALREADY_EXISTS(409, "AccountId Already Exists");
+    ACCOUNTED_ALREADY_EXISTS(409, "AccountId Already Exists"),
+    USER_NOT_FOUND(404,"존재하지 않는 유저입니다. ");
 
     private final Integer httpStatus;
     private final String message;
