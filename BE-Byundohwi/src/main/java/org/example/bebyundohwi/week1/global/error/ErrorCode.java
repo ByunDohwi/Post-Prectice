b4db95da69b67e534a4fa23a3d59e4ffc6cd4aa0
ByunDohwi <dohwe0978@gmail.com>
2024-08-11 05:12:27 +0900

    feat :: InvaildTokenException

--- BE-Byundohwi/src/main/java/org/example/bebyundohwi/week1/global/error/ErrorCode.java
+++ BE-Byundohwi/src/main/java/org/example/bebyundohwi/week1/global/error/ErrorCode.java
@@ -9,8 +9,11 @@ public enum ErrorCode {
 
 
     USER_ALREADY_EXISTS(409, "Username Already Exists"),
+    USER_NOT_FOUND(404,"존재하지 않는 유저입니다. "),
     ACCOUNTED_ALREADY_EXISTS(409, "AccountId Already Exists"),
-    USER_NOT_FOUND(404,"존재하지 않는 유저입니다. ");
+
+    INVALID_TOKEN(401,"Invalid Token."),
+    EXPIRED_TOKEN(401, "Expired Token");
 
     private final Integer httpStatus;
     private final String message;
