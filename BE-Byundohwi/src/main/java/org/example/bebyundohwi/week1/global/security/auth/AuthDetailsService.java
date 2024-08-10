b1082a5cb0cdaed36b6afe0b7b0a7deb20a0ade1
ByunDohwi <dohwe0978@gmail.com>
2024-08-11 01:23:59 +0900

    feat :: Auth

---
+++ BE-Byundohwi/src/main/java/org/example/bebyundohwi/week1/global/security/auth/AuthDetailsService.java
@@ -0,0 +1,20 @@
+package org.example.bebyundohwi.week1.global.security.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.example.bebyundohwi.week1.domain.user.domain.UserEntity;
+import org.example.bebyundohwi.week1.domain.user.facade.UserFacade;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AuthDetailsService implements UserDetailsService {
+    private final UserFacade userFacade;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user = userFacade.getUserByUserName(username);
+        return new AuthDetails(user.getUsername());
+    }
+}
