c9ebf25b278f65e1f6afe7ec890e6ec706711e3b
ByunDohwi <dohwe0978@gmail.com>
2024-09-10 14:02:36 +0900

    fix :: admin, user login 수정

--- BE-Byundohwi/src/main/java/org/example/bebyundohwi/week1/domain/admin/service/AdminLoginService.java
+++ BE-Byundohwi/src/main/java/org/example/bebyundohwi/week1/domain/admin/service/AdminLoginService.java
@@ -8,6 +8,7 @@ import org.example.bebyundohwi.week1.domain.admin.repository.AdminRepository;
 import org.example.bebyundohwi.week1.domain.user.exception.UserNotFoundException;
 import org.example.bebyundohwi.week1.global.security.jwt.JwtTokenProvider;
 import org.example.bebyundohwi.week1.global.security.jwt.dto.TokenResponse;
+import org.example.bebyundohwi.week1.global.security.role.Role;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class AdminLoginService {
     public TokenResponse login(AdminSignupRequest request) {
         AdminEntity admin = adminRepository.findByAdminName(request.getAdminName()).orElseThrow(() -> UserNotFoundException.EXCEPTION);
         if (passwordEncoder.matches(request.getPassword(),admin.getPassword())) {//matches(인코드 안한 비번, 인코드 한 비번)
-            return jwtTokenProvider.generateToken(request.getAdminName(), admin.getRole());
+            return jwtTokenProvider.generateToken(request.getAdminName(), Role.ADMIN.toString());
         }
         else {
             throw AdminMissMatchException.EXCEPTION;
